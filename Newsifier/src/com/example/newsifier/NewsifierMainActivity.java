72ab5a3604ad02aff10b78ac902143b01ed058ee
Prasenjit Sharan <prasenjit.sharan@gmail.com>
2013-02-27 22:59:28 +0530

    Imported web version files from prasenjitsharan.com

--- Newsifier/src/com/example/newsifier/NewsifierMainActivity.java
+++ Newsifier/src/com/example/newsifier/NewsifierMainActivity.java
@@ -11,7 +11,7 @@ public class NewsifierMainActivity extends DroidGap {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         /*setContentView(R.layout.activity_newsifier_main);*/
-        super.loadUrl("file:///android_asset/www/index.html");
+        super.loadUrl("file:///android_asset/www/alpha/index.html");
     }
 
     @Override
